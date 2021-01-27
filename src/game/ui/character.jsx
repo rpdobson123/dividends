540ad7c6b0c5bfda6231987f4ec545aeb259ae7f
Richard Dobson <rpg.zealot@gmail.com>
2021-01-26 21:58:57 -0700

    add signature

--- src/game/ui/character.jsx
+++ src/game/ui/character.jsx
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from "react";
 export const Character = (props) => {
   const { character } = props;
   if (!character) return "";
-  const { name } = character;
+  const { name, image } = character;
   return (
     <div className={`character-container`}>
       <div className="character-container-image">
-        <img src={character.image} />
+        <img src={image} />
       </div>
-      <div className="character-container-name">{character.name}</div>
+      <div className="character-container-name">{name}</div>
     </div>
   );
 };
